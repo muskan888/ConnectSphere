5906b5e02eb28c17d7b85f449ff6d741903ee7a0
pankaj jakhar <pankaj.jakhar@vidhema.com>
2021-07-11 15:24:55 +0530

    changes

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const chat_controller = require("./controllers/chatController");
+const chat_controller = require("./Controllers/chatController");
 
 router.get("/chat", chat_controller.get);
 router.get("/chat_chat", chat_controller.getAll);
